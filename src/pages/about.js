7bc4bdc760e89f193413418ca7635f17fdb154e8
Jay <m15205519363@163.com>
2020-11-11 16:24:09 +0800

    initial Jay`s blog

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>About me</h1>
+        <p>I am a good by, I`m smart enough, and gosh darn it, prople lile me!</p>
+    </Layout>
+
+)
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>I'd love to talk! Email me at the address below</h1>
+        <p>
+            <a href="www.baidu.com">contact me!</a>
+        </p>
+    </Layout>
+
+)
